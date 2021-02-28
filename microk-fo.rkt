680462685ce7e7e2147fbb6f864cdd1352a30a65
Ende Jin <jinende1997@gmail.com>
2021-02-28 18:22:09 -0500

    Adapting on sem-solving.

--- microk-fo.rkt
+++ microk-fo.rkt
@@ -1021,24 +1021,99 @@
         (wrap-state-stream new-st-w/pf)))
 
 
-    ((symbolo t1)  (wrap-state-stream (check-as-inf-type symbol? t1 (prim-goal-filled-st st g))))
+    ((symbolo t1)  
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = (set symbol?)]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mapped-stream do-each-state stream))
     ((not-symbolo t1) 
-      (mplus 
-        (term-finite-type asumpt t1 (prim-goal-filled-st (st . <-pfg . (_ ignore) _) g))
-        (wrap-state-stream (check-as-inf-type-disj (set-remove all-inf-type-label symbol?) t1 (prim-goal-filled-st st g))))) 
-    ((numbero t1) (wrap-state-stream (check-as-inf-type number? t1 (prim-goal-filled-st st g))))
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = (set-remove all-inf-type-label symbol?)]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mplus 
+            (term-finite-type asumpt t1 (prim-goal-filled-st (st . <-pfg . (_ ignore) _) g))
+            (mapped-stream do-each-state stream) ))) 
+
+    ((numbero t1) 
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = (set number?)]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mapped-stream do-each-state stream)))
     ((not-numbero t1)  
-      (mplus 
-        (term-finite-type asumpt t1 (prim-goal-filled-st (st . <-pfg . (_ ignore) _) g))
-        (wrap-state-stream (check-as-inf-type-disj (set-remove all-inf-type-label number?) t1 (prim-goal-filled-st st g))))) 
-    ((stringo t1) (wrap-state-stream (check-as-inf-type string? t1 (prim-goal-filled-st st g))))
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = (set-remove all-inf-type-label symbol?)]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mplus 
+            (term-finite-type asumpt t1 (prim-goal-filled-st (st . <-pfg . (_ ignore) _) g))
+            (mapped-stream do-each-state stream) ))) 
+    ((stringo t1) 
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = (set string?)]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mapped-stream do-each-state stream)))
+    
     ((not-stringo t1)  
-      (mplus 
-        (term-finite-type asumpt t1 (prim-goal-filled-st (st . <-pfg . (_ ignore) _) g))
-        (wrap-state-stream (check-as-inf-type-disj (set-remove all-inf-type-label string?) t1 (prim-goal-filled-st st g)))))
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = (set-remove all-inf-type-label string?)]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mplus 
+            (term-finite-type asumpt t1 (prim-goal-filled-st (st . <-pfg . (_ ignore) _) g))
+            (mapped-stream do-each-state stream) )))
 
     ((type-constraint t types)
-      (wrap-state-stream (check-as-inf-type-disj types t (prim-goal-filled-st st g))))
+        (match-let*
+            [stream (pause st (not-finite-type t1))]
+            [do-each-state
+              (λ (st) 
+                (run-st st 
+                  (do 
+                    [type-info  = types]
+                    [a:type-cst <- (fresh-param (term) (add-to-tha (type-constraint t1 type-info) ))]
+                    [t:type-cst <- (check-as-inf-type-disj/state t1 a:type-cst)]
+                    [_ <- (pf-filled t:type-cst)])))]
+          (mapped-stream do-each-state stream)))
 
     ((ex v gn) 
       ;;; TODO: make scope a ordered set (or just a list)
